0e7359aeee51cc35b1dedf00b69f89013278ef6a
rodrigocortezdev <rodrigocortezdev@gmail.com>
2021-01-29 19:51:43 -0300

    Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DatePicker, Button, Upload } from 'antd';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{ display: 'flex', flexDirection: 'row' }}>
+			<DatePicker />
+			<Button size={'large'} shape={'round'} type={'primary'}>
+				Teste
+			</Button>
+			<Upload type={'select'}>aaaaaaaaaaaaaa</Upload>
+		</div>
+	);
 }
 
 export default App;
