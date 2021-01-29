b13dea6e9492b447c342b86578c16017f6670c24
rodrigocortezdev <rodrigocortezdev@gmail.com>
2021-01-29 20:57:29 -0300

    Ajustes de estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { DatePicker, Button, Upload } from 'antd';
+import Routes from './routes';
 
 function App() {
-	return (
-		<div style={{ display: 'flex', flexDirection: 'row' }}>
-			<DatePicker />
-			<Button size={'large'} shape={'round'} type={'primary'}>
-				Teste
-			</Button>
-			<Upload type={'select'}>aaaaaaaaaaaaaa</Upload>
-		</div>
-	);
+	return <Routes />;
 }
 
 export default App;
