5f7bb08ff7e169c50f34402afcd7f886e554ffaf
rodrigocortezdev <rodrigocortezdev@gmail.com>
2021-01-30 16:11:59 -0300

    Master e Ajustes

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import * as Sc from './styled';
+import Master from '../Master';
 
 export default function Home() {
-	return <div>Master</div>;
+	return (
+		<Master headerText="Home page">
+			<Sc.Container>Home</Sc.Container>
+		</Master>
+	);
 }
