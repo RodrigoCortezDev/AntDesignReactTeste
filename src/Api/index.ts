dd3a8d393b7c63f413edc56a2418a93cd89d26e0
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2021-05-26 09:04:49 -0300

    teste

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
-const base = 'http://localhost:56003/';
-//const base = 'https://localhost:56003';
+//const base = 'http://localhost:56003/';
+const base = 'https://localhost:56003';
 //const base = 'http//localhost:3333';
 
 const Api = Axios.create({
